685203e654ef31cd252f8bd7396690d1d0fedcef
loopy-lim <loopy@jnu.ac.kr>
2023-04-24 22:50:25 +0900

    fix(app): fix app name and tailwind
    fix app name: jnu-pin to jnu-pia(Portal Intelligent Assistant)
    tailwind: add postcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import InputText from "./component/chat/ChatInputBox";
+
+const App = () => {
   return (
     <>
-      <h1>JNU-PIN</h1>
+      <h1>JNU-PIA</h1>
+      <div className="p-4">
+        <InputText />
+      </div>
     </>
   );
-}
+};
 
 export default App;
